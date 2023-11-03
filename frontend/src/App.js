3d8a7691a9c7e8bc7607330decf0b8b261fdd232
msalmanKT <muhammad.salman@gomotive.com>
2023-11-04 00:34:31 +0500

    Added Header and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      welcome to proshop
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
